3cc22fefd954a20b3421ef0896eb293911435d4e
Richard Simko <richard.simko@icloud.com>
2015-04-06 11:38:06 +0200

    Removed bin and also lab 4

--- src/eda095/lab1/Parser.java
+++ src/eda095/lab1/Parser.java
@@ -7,59 +7,67 @@ import java.net.URL;
 import java.util.ArrayList;
 
 public class Parser {
-	public static ArrayList<String> getHrefs(String urlIn) {
-		ArrayList<String> hrefs = new ArrayList<String>(); 
-		URL url;
-		try {
-			url = new URL(urlIn);
-		} catch (MalformedURLException e) {
-			return hrefs;
-		}
-		char[] troll = "<a href=\"".toCharArray();
-		InputStream hip;
-		try {
-			hip = (InputStream)url.getContent();
-		} catch (IOException e) {
-			return hrefs;
-		}
-		int state = 0;
-		int c = 0;
-		for(;c != -1;) {
-			try {
-				c = hip.read();
-			} catch (IOException e) {
-				return hrefs;
-			}
-			if( c == -1)
-				break;
-			if(troll[state] == c){
-				state = ++state % troll.length;
-				if(state == 0) {
-					StringBuilder sb = new StringBuilder();					
-					try {
-						c = hip.read();
-					} catch (IOException e) {
-						return hrefs;
-					}
-					while( c != -1 && c != '"') {
-						sb.append((char)c);
-						try {
-							c = hip.read();
-						} catch (IOException e) {
-							return hrefs;
-						}
-					}
-					hrefs.add(sb.toString());
-					state = 0;
-				}
-			} else if(Character.isWhitespace(c)){
-					continue;
-			} else {
-				if( state>3) state = 3;
-				else if( state>0) state = 0;
-			}
-			
-		}
-		return hrefs;
-	}
+    public static ArrayList<String> getHrefs(String urlIn) {
+        ArrayList<String> hrefs = new ArrayList<String>();
+        URL url;
+        try {
+            url = new URL(urlIn);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return hrefs;
+        }
+        char[] troll = "<a href=\"".toCharArray();
+        InputStream hip;
+        try {
+            hip = (InputStream) url.getContent();
+        } catch (IOException e) {
+            return hrefs;
+        }
+        int state = 0;
+        int c = 0;
+        for (; c != -1;) {
+            try {
+                c = hip.read();
+            } catch (IOException e) {
+                return hrefs;
+            }
+            if (c == -1)
+                break;
+            if (troll[state] == c) {
+                state = ++state % troll.length;
+                if (state == 0) {
+                    StringBuilder sb = new StringBuilder();
+                    try {
+                        c = hip.read();
+                    } catch (IOException e) {
+                        return hrefs;
+                    }
+                    while (c != -1 && c != '"') {
+                        sb.append((char) c);
+                        try {
+                            c = hip.read();
+                        } catch (IOException e) {
+                            return hrefs;
+                        }
+                    }
+                    String newUrl = sb.toString();
+                    if (newUrl.startsWith("http") || newUrl.startsWith("https") || newUrl.startsWith("www")) {
+//                        System.out.println("Absolute");
+                    } else
+                        newUrl = url.getProtocol() + "://" + url.getHost() + "/" + newUrl;
+                    hrefs.add(newUrl);
+                    state = 0;
+                }
+            } else if (Character.isWhitespace(c)) {
+                continue;
+            } else {
+                if (state > 3)
+                    state = 3;
+                else if (state > 0)
+                    state = 0;
+            }
+
+        }
+        return hrefs;
+    }
 }
---
+++ src/eda095/lab4/main/Main.java
@@ -0,0 +1,21 @@
+package eda095.lab4.main;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import eda095.lab2.sd.SD;
+import eda095.lab4.thread.Runner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SD sharedData = new SD();
+        int numWorkers = 10;
+        ExecutorService e = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < numWorkers; i++) {
+            e.submit(new Runner(sharedData));
+        }
+        sharedData.putUrl("http://cs.lth.se/pierre_nugues/");
+    }
+
+}
